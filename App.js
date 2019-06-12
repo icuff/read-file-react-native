dd6fe80b9321caaf0189a5af2996bf4580edbf13
bc-nogueira <bcnogueira@id.uff.br>
2019-06-11 22:28:40 -0300

    Add timer.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Button, StyleSheet, Text, View, ToastAndroid, PermissionsAndroid } from 'react-native';
 import * as RNFS from 'react-native-fs';
+import moment from 'moment';
 
 export default class App extends Component {
   state = {
@@ -24,17 +25,19 @@ export default class App extends Component {
   }
 
   readFile = async () => {
+    const timerBegin = moment().valueOf();
     this.requestPermission();
     downloadDir = RNFS.ExternalStorageDirectoryPath + '/Download/';
-    RNFS.readFile(downloadDir + 'hello.txt', 'ascii').then(content => {
+    RNFS.readFile(downloadDir + 'testReactNative.txt', 'ascii').then(content => {
       this.setState({
         fileContent: content,
         showReadBtn: false
       });
-    })
-      .catch(err => {
-        ToastAndroid.show(err.message + ' - ' + err.code, ToastAndroid.SHORT);
-      });
+      const timerEnd = moment().valueOf();
+      ToastAndroid.show('Finished in ' + (timerEnd - timerBegin) + 'ms', ToastAndroid.LONG);
+    }).catch(err => {
+      ToastAndroid.show(err.message + ' - ' + err.code, ToastAndroid.SHORT);
+    });
   };
 
   render() {
